aab1e3911e0e56560e407c28d43a34967230c54f
loic.postec <loic.postec@heig-vd.ch>
2020-01-09 02:01:29 +0100

    change add bench interface, link photo upload with the api, prepared function for options select and benchUpload

--- src/app/add-bench/add-bench.page.ts
+++ src/app/add-bench/add-bench.page.ts
@@ -6,6 +6,7 @@ import { ImagePicker } from '@ionic-native/image-picker/ngx';
 import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer/ngx';
 import { QimgImage } from '../models/qimg-image';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { PictureService } from '../services/picture/picture.service'
 
 @Component({
   selector: 'app-add-bench',
@@ -20,13 +21,15 @@ export class AddBenchPage implements OnInit {
   results: any;
   image: string;
   pictureData: string;
+  picture: QimgImage;
 
   constructor(
     private geolocation: Geolocation,
     private imagePicker: ImagePicker,
     private crop: Crop,
     private transfer: FileTransfer,
-    private camera: Camera
+    private camera: Camera,
+    private pictureService: PictureService,
   ) {
 
     this.mapOptions = {
@@ -67,19 +70,15 @@ export class AddBenchPage implements OnInit {
         console.log("changed rating: ",rating);
         // do your stuff
     }
-    
-    takePicture() {
-      const options: CameraOptions = {
-        quality: 100,
-        destinationType: this.camera.DestinationType.DATA_URL,
-        encodingType: this.camera.EncodingType.JPEG,
-        mediaType: this.camera.MediaType.PICTURE
-      };
-      this.camera.getPicture(options).then(pictureData => {
-        this.pictureData = pictureData;
-      }).catch(err => {
-        console.warn(`Could not take picture because: ${err.message}`);
+
+  takePicture() {
+         this.pictureService.takeAndUploadPicture().subscribe(picture => {
+        this.picture = picture;
+      }, err => {
+        console.warn('Could not take picture', err);
       });
     }
-  
+    uploadBench() {
+      console.log("UP")
+}
 };
