6434fcf9fe0495ce27d965573ef03e3494a0a8fd
loic.postec <loic.postec@heig-vd.ch>
2020-01-13 16:19:07 +0100

    profile and own bench management

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { User } from '../models/user';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Bench } from '../models/bench';
+import { BenchPage } from '../models/benchPage';
 
 @Component({
   selector: 'app-profile',
@@ -11,6 +14,7 @@ import { HttpClient } from '@angular/common/http';
 export class ProfilePage implements OnInit {
   userId = null;
   username = null;
+  benches: Bench[];
   constructor(private storage: Storage,    public http: HttpClient ) { }
  
   ngOnInit() {
@@ -21,6 +25,22 @@ export class ProfilePage implements OnInit {
       this.username=val;
     })
 
+      const benchesUrl = `${environment.apiUrl}/benches`
+    this.http.get<BenchPage>(benchesUrl).subscribe(result => {
+      console.log(`Benches loaded`, result);
+    this.benches = result.data.filter(data => data.userId = this.userId );
+      console.log(this.benches);
+    });
   }
 
+  deleteBench(benchId){
+    console.log(benchId)
+    const benchesUrl = `${environment.apiUrl}/benches/`+benchId;
+    this.http.delete(benchesUrl).subscribe(result => {
+      console.log(`Benches deleted`, result);
+      location.reload();
+  })
 }
+}
+
+
